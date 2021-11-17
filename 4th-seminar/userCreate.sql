5e3a336304b2b33d417afc8e207cbeb9acff7768
eojinsarang joo <wndjwlstkfkd@naver.com>
2021-11-18 02:48:17 +0900

    [FEAT] : 4th-seminar

---
+++ 4th-seminar/userCreate.sql
@@ -0,0 +1,13 @@
+create table "user"
+(
+	id serial
+		constraint user_pk
+			primary key,
+	email varchar(100),
+	id_firebase varchar(300),
+	username varchar(50) not null,
+	phone varchar(15),
+	created_at timestamp default now(),
+	updated_at timestamp default now(),
+	"is_deleted()" boolean default false
+);
